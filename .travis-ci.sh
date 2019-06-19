10e3bb311ac3f075a6f972c8fb6549ff0a1b67e0
Simon Shine <shreddedglory@gmail.com>
2019-06-19 08:38:34 +0200

    Testing Travis CI with plain Ubuntu commands in .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-ocaml -version
+echo "yes" | add-apt-repository ppa:avsm/ppa
+apt -qq update
+apt -qq install opam
+
 opam --version
 
-opam init
-eval $(opam config env)
-opam install "ocamlfind core base ounit qcheck react ppx_deriving"
+opam init -a
+eval $(opam env)
+opam install dune ocamlfind core base ounit qcheck react ppx_deriving
+
+ocaml -version
 
 make test
