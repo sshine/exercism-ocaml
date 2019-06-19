d3098d249233260bd1c7694b77b276b712b99d5d
Simon Shine <shreddedglory@gmail.com>
2019-06-19 16:42:33 +0200

    Trying to add .travis-ocaml.env file during Travis CI run

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+OCAML_VERSION=4.08.0
+
 sudo apt install m4
+
+echo OCAML_VERSION=$OCAML_VERSION >  .travis-ocaml.env
+echo OPAM_SWITCH=$OCAML_VERSION   >> .travis-ocaml.env
 
 opam --version
 ocaml -version
